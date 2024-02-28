6dc9f4ac061cfe850054c9a4bb56418e801343f1
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-02-28 17:57:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
     //Write your code here
-  
+let tabelbody=documentById("sampleTable")
+	let btn=document.querySelector('button')
+
+	btn.addEventListener('click',addrow)
+	function addRow() {
+		let row=tabelbody.insertRow(0)
+		row.insertCell(0).innerHTML="New Cell1"
+		row.insertCell(1).innerHTML="New Cell2"
+	}
+	
+	
   
 }
+
+addRow({"New Cell1","New Cell2"})
